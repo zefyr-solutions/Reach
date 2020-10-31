813919365f3b91e4e91881db5fa75d22a43854b9
Raza <razabinashraf@gmail.com>
2020-10-31 12:15:24 +0530

    Merge branch 'development' of https://github.com/zefyr-solutions/Reach into development

--- main.py
+++ main.py
@@ -52,74 +52,83 @@ def home():
 
 @app.route("/add_user", methods=["POST","GET"])
 def add_user():
-    if request.method == 'POST':
+    if 'user_id' in session:
+        if request.method == 'POST':
 
-        if (request.form["name"] == "" or request.form["username"] == "" or request.form["email"] == "" or request.form["phone"] == "" or request.form["password"] == "" or request.form["cpassword"] == ""  or request.form["role"] == ""):
-            flash("Enter all the fields")
-            return render_template("add_user.html")
-        else:
-            userDetails = request.form
-            name = userDetails['name']
-            username = userDetails['username']
-            email = userDetails['email']
-            phone = userDetails['phone']
-            role = userDetails['role']
-            password = userDetails['password']
-            cpassword = userDetails['cpassword']
-            if password == cpassword:
-                xpassword = argon2.hash(password)
-                cur = mysql.connection.cursor()
-                cur.execute("INSERT INTO users(user_name,name,email,phone_no,role,password) VALUES(%s,%s,%s,%s,%s,%s)",(username,name,email,phone,role,xpassword))
-                mysql.connection.commit()
-                cur.close()
-                flash("Records inserted")
+            if (request.form["name"] == "" or request.form["username"] == "" or request.form["email"] == "" or request.form["phone"] == "" or request.form["password"] == "" or request.form["cpassword"] == ""  or request.form["role"] == ""):
+                flash("Enter all the fields")
                 return render_template("add_user.html")
             else:
-                flash("Both passwords must be same")
-                return render_template("add_user.html")
+                userDetails = request.form
+                name = userDetails['name']
+                username = userDetails['username']
+                email = userDetails['email']
+                phone = userDetails['phone']
+                role = userDetails['role']
+                password = userDetails['password']
+                cpassword = userDetails['cpassword']
+                if password == cpassword:
+                    xpassword = argon2.hash(password)
+                    cur = mysql.connection.cursor()
+                    cur.execute("INSERT INTO users(user_name,name,email,phone_no,role,password) VALUES(%s,%s,%s,%s,%s,%s)",(username,name,email,phone,role,xpassword))
+                    mysql.connection.commit()
+                    cur.close()
+                    flash("Records inserted")
+                    return render_template("add_user.html")
+                else:
+                    flash("Both passwords must be same")
+                    return render_template("add_user.html")
+        else:
+                    return render_template("add_user.html")
     else:
-        return render_template("add_user.html")
+        return "Login first"
 
 @app.route("/add_product", methods=["POST","GET"])
 def add_product():
-    if request.method == 'POST':
+    if 'user_id' in session:
+        if request.method == 'POST':
 
-        if (request.form["pname"] == "" or request.form["bcode"] == "" or request.form["price"] == "" ):
-            flash("Enter all the fields")
+            if (request.form["pname"] == "" or request.form["bcode"] == "" or request.form["price"] == "" ):
+                flash("Enter all the fields")
+            else:
+                userDetails = request.form
+                pname = userDetails['pname']
+                bcode = userDetails['bcode']
+                price = userDetails['price']
+                cur = mysql.connection.cursor()
+                cur.execute("INSERT INTO products(name,barcode,price) VALUES(%s,%s,%s)",(pname,bcode,price))
+                mysql.connection.commit()
+                cur.close()
+                flash("Records inserted")
+
         else:
-            userDetails = request.form
-            pname = userDetails['pname']
-            bcode = userDetails['bcode']
-            price = userDetails['price']
-            cur = mysql.connection.cursor()
-            cur.execute("INSERT INTO products(name,barcode,price) VALUES(%s,%s,%s)",(pname,bcode,price))
-            mysql.connection.commit()
-            cur.close()
-            flash("Records inserted")
-
+            return render_template("add_product.html")
     else:
-        return render_template("add_product.html")
+        return "Login first"
 
 @app.route("/add_customer", methods=["POST","GET"])
 def add_customer():
-    if request.method == 'POST':
+    if 'user_id' in session:
+        if request.method == 'POST':
 
-        if (request.form["name"] == "" or request.form["phone_no"] == "" or request.form["email"] == "" or request.form["location"] == ""):
-            flash("Enter all the fields")
+            if (request.form["name"] == "" or request.form["phone_no"] == "" or request.form["email"] == "" or request.form["location"] == ""):
+                flash("Enter all the fields")
+            else:
+                userDetails = request.form
+                name = userDetails['name']
+                phone_no = userDetails['phone_no']
+                email = userDetails['email']
+                location = userDetails['location']
+                cur = mysql.connection.cursor()
+                cur.execute("INSERT INTO customers(name,phone,email,location) VALUES(%s,%s,%s,%s)",(name,phone_no,email,location))
+                mysql.connection.commit()
+                cur.close()
+                flash("Records inserted")
+
         else:
-            userDetails = request.form
-            name = userDetails['name']
-            phone_no = userDetails['phone_no']
-            email = userDetails['email']
-            location = userDetails['location']
-            cur = mysql.connection.cursor()
-            cur.execute("INSERT INTO customers(name,phone,email,location) VALUES(%s,%s,%s,%s)",(name,phone_no,email,location))
-            mysql.connection.commit()
-            cur.close()
-            flash("Records inserted")
-
+            return render_template("add_customer.html")
     else:
-        return render_template("add_customer.html")
+        return "Login First"
 
 @app.route("/driver")
 def driver():
@@ -128,78 +137,162 @@ def driver():
 
 @app.route("/view_user")
 def view_user():
-    if request.args.get("customer_id") :
-        customer_id = request.args.get("customer_id")
-        cur = mysql.connection.cursor()
-        cur.execute("SELECT name,email,phone,loaction FROM customers where customer_id = %s ",[customer_id])
-        row = cur.fetchone()
-        return render_template("view_user_specific.html")
-    else :
-        cur = mysql.connection.cursor()
-        cur.execute("SELECT user_name,name,email,phone_no,role,user_id FROM users")
-        rows = cur.fetchall()
-        return render_template("view_user.html", value=rows)
+    if 'user_id' in session:
+        if request.args.get("customer_id") :
+            customer_id = request.args.get("customer_id")
+            cur = mysql.connection.cursor()
+            cur.execute("SELECT name,email,phone,loaction FROM customers where customer_id = %s ",[customer_id])
+            row = cur.fetchone()
+            return render_template("view_user_specific.html")
+        else :
+            cur = mysql.connection.cursor()
+            cur.execute("SELECT user_name,name,email,phone_no,role,user_id FROM users")
+            rows = cur.fetchall()
+            return render_template("view_user.html", value=rows)
+    else:
+        return "Login First"
 
 @app.route("/view_customer")
 def view_customer():
-    if request.args.get("customer_id") :
-        customer_id = request.args.get("customer_id")
-        cur = mysql.connection.cursor()
-        cur.execute("SELECT name,email,phone,location,customer_id FROM customers where customer_id = %s ",[customer_id])
-        row = cur.fetchone()
-        return render_template("view_customer_specific.html", row=row)
+    if 'user_id' in session:
+        if request.args.get("customer_id") :
+            customer_id = request.args.get("customer_id")
+            cur = mysql.connection.cursor()
+            cur.execute("SELECT name,email,phone,location,customer_id FROM customers where customer_id = %s ",[customer_id])
+            row = cur.fetchone()
+            return render_template("view_customer_specific.html", row=row)
+        else :
+            cur = mysql.connection.cursor()
+            cur.execute("SELECT name,email,phone,location,customer_id FROM customers")
+            rows = cur.fetchall()
+            return render_template("view_customer.html", value=rows)
     else :
-        cur = mysql.connection.cursor()
-        cur.execute("SELECT name,email,phone,location,customer_id FROM customers")
-        rows = cur.fetchall()
-        return render_template("view_customer.html", value=rows)
+        return "Login First"
 
 @app.route("/view_product")
 def view_product():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT name,barcode,price FROM products")
-    rows = cur.fetchall()
-    return render_template("view_product.html", value=rows)
+    if 'user_id' in session:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT name,barcode,price FROM products")
+        rows = cur.fetchall()
+        return render_template("view_product.html", value=rows)
+    else:
+        return "Login First"
+
 
 @app.route("/edit_user/<user_id>", methods=["POST","GET"])
 def edit_user(user_id):
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT name,user_name,email,phone_no,role,password FROM users where user_id = %s",user_id)
-    rows = cur.fetchone()
-    cur.close()
+    if 'user_id' in session:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT name,user_name,email,phone_no,role,password FROM users where user_id = %s",user_id)
+        rows = cur.fetchone()
+        cur.close()
 
-    if request.method == 'POST':
-        if (request.form["name"] == "" or request.form["username"] == "" or request.form["email"] == "" or request.form["phone"] == "" or request.form["password"] == "" or request.form["npassword"] == "" or request.form["cpassword"] == ""  or request.form["role"] == ""):
-            flash("Enter all the fields")
-            return redirect(url_for('edit_user',user_id=user_id))
-        else:
-            userDetails = request.form
-            name = userDetails['name']
-            username = userDetails['username']
-            email = userDetails['email']
-            phone = userDetails['phone']
-            role = userDetails['role']
-            password = userDetails['password']
-            npassword = userDetails['npassword']
-            cpassword = userDetails['cpassword']
-            if (argon2.verify(password,rows[5])):
-                if npassword == cpassword:
-                    xpassword = argon2.hash(npassword)
-                    cur = mysql.connection.cursor()
-                    cur.execute("UPDATE users SET user_name = %s,name = %s,email = %s,phone_no = %s,role = %s,password = %s where user_id = %s",(username,name,email,phone,role,xpassword,user_id) )
-                    mysql.connection.commit()
-                    cur.close()
-                    flash("Records updated")
-                else:
-                    flash("Both passwords must be same")
-                    return redirect(url_for('edit_user',user_id=user_id))
-            else:
-                flash("Current password is not verified")
+        if request.method == 'POST':
+            if (request.form["name"] == "" or request.form["username"] == "" or request.form["email"] == "" or request.form["phone"] == "" or request.form["password"] == "" or request.form["npassword"] == "" or request.form["cpassword"] == ""  or request.form["role"] == ""):
+                flash("Enter all the fields")
                 return redirect(url_for('edit_user',user_id=user_id))
-    else:
-        return render_template("edit_user.html",value=rows, user_id=user_id)
+            else:
+                userDetails = request.form
+                name = userDetails['name']
+                username = userDetails['username']
+                email = userDetails['email']
+                phone = userDetails['phone']
+                role = userDetails['role']
+                password = userDetails['password']
+                npassword = userDetails['npassword']
+                cpassword = userDetails['cpassword']
+                if (argon2.verify(password,rows[5])):
+                    if npassword == cpassword:
+                        xpassword = argon2.hash(npassword)
+                        cur = mysql.connection.cursor()
+                        cur.execute("UPDATE users SET user_name = %s,name = %s,email = %s,phone_no = %s,role = %s,password = %s where user_id = %s",(username,name,email,phone,role,xpassword,user_id) )
+                        mysql.connection.commit()
+                        cur.close()
+                        flash("Records updated")
+                    else:
+                        flash("Both passwords must be same")
+                        return redirect(url_for('edit_user',user_id=user_id))
+                else:
+                    flash("Current password is not verified")
+                    return redirect(url_for('edit_user',user_id=user_id))
+        else:
+            return render_template("edit_user.html",value=rows, user_id=user_id)
 
-    return redirect(url_for('view_user'))
+        return redirect(url_for('view_user'))
+    else:
+        return "Login First"
+
+@app.route("/edit_product/<product_id>", methods=["POST","GET"])
+def edit_product(product_id):
+    if 'user_id' in session:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT name,barcode,price,product_id FROM products where product_id = %s",product_id)
+        rows = cur.fetchone()
+
+        if request.method == 'POST':
+
+            if (request.form["pname"] == "" or request.form["bcode"] == "" or request.form["price"] == "" ):
+                flash("Enter all the fields")
+                return render_template("edit_product.html",value=rows ,product_id=product_id)
+
+            else:
+                userDetails = request.form
+                pname = userDetails['pname']
+                bcode = userDetails['bcode']
+                price = userDetails['price']
+                cur = mysql.connection.cursor()
+                cur.execute("UPDATE products SET name = %s,barcode = %s,price = %s where product_id = %s",(pname,bcode,price,product_id))
+                mysql.connection.commit()
+                cur.close()
+                flash("Records Updated")
+                return redirect(url_for('view_user'))
+
+        else:
+            return render_template("edit_product.html",value=rows ,product_id=product_id)
+    else:
+        return "Login first"
+
+@app.route("/edit_customer/<customer_id>", methods=["POST","GET"])
+def edit_customer(customer_id):
+    if 'user_id' in session:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT name,email,phone,location,customer_id FROM customers where customer_id = %s ",customer_id)
+        row = cur.fetchone()
+        if request.method == 'POST':
+
+            if (request.form["name"] == "" or request.form["phone_no"] == "" or request.form["email"] == "" or request.form["location"] == ""):
+                flash("Enter all the fields")
+                return render_template("edit_customer.html",value=rows, customer_id=customer_id)
+            else:
+                userDetails = request.form
+                name = userDetails['name']
+                phone_no = userDetails['phone_no']
+                email = userDetails['email']
+                location = userDetails['location']
+                cur = mysql.connection.cursor()
+                cur.execute("UPDATE customers SET name = %s,phone = %s,email = %s,location = %s where customer_id = %s",(name,phone_no,email,location,customer_id))
+                mysql.connection.commit()
+                cur.close()
+                flash("Records updated")
+                return redirect(url_for('view_customer'))
+
+        else:
+            return render_template("edit_customer.html",value=row, customer_id=customer_id)
+    else:
+        return "Login First"
+
+
+@app.route("/logout")
+def logout():
+    if 'user_id' in session:
+        session.pop('user_id',None)
+
+    if 'role' in session:
+        session.pop('role',None)
+
+    return redirect(url_for('home'))
+
 
 
 if __name__ == "__main__":
